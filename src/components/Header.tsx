65d0f6cee9ff7419fd0b612f1134b8dd941709ea
Vinicius Cole de Amorim <viniamorim87@hotmail.com>
2023-04-17 17:17:13 -0300

    renato albani

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,58 @@
-import styles from '../styles/components/Header.module.css'
+import styles from "../styles/components/Header.module.css";
 
-import { SearchBar } from './SearchBar'
+import { SearchBar } from "./SearchBar";
+
+import { TiShoppingCart } from "react-icons/ti";
 
 export function Header() {
+  const abrirMenuProdutores = () => {
+    const menuProdutores = document.querySelector(
+      `.${styles.menuProdutores}`
+    ) as HTMLDivElement;
+    menuProdutores.style.display = "flex";
+    menuProdutores.classList.add(styles.slideintop);
+  };
 
-    const abrirMenuProdutores = () => {
-        const menuProdutores = document.querySelector(`.${styles.menuProdutores}`) as HTMLDivElement
-        menuProdutores.style.display = "flex"
-        menuProdutores.classList.add(styles.slideintop)
-    }
+  const fecharMenuProdutores = () => {
+    const menuProdutores = document.querySelector(
+      `.${styles.menuProdutores}`
+    ) as HTMLDivElement;
+    menuProdutores.style.display = "none";
+  };
 
-    const fecharMenuProdutores = () => {
-        const menuProdutores = document.querySelector(`.${styles.menuProdutores}`) as HTMLDivElement
-        menuProdutores.style.display = "none"
-    }
+  const ativaFiltroProdutor = (evento: React.MouseEvent<HTMLDivElement>) => {
+    evento.currentTarget.classList.toggle(styles.ativo);
+    //todo: adicionar filtro
+  };
 
-    const ativaFiltroProdutor = (evento: React.MouseEvent<HTMLDivElement>) => {
-        evento.currentTarget.classList.toggle(styles.ativo)
-        //todo: adicionar filtro
-    }
+  const produtores = ["Vanildo", "João", "Maria", "José", "ASdmiosajiod"];
 
-
-
-    const produtores = [ "Vanildo", "João", "Maria", "José", "ASdmiosajiod" ]
-
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerConteudo}>
-                <img src="/icone-astral.png" alt="Astral logo" />
-                <a href="/">Início</a>
-                <a href="/sobre">Sobre</a>
-                <SearchBar />
-                <section onMouseEnter={abrirMenuProdutores} onMouseLeave={fecharMenuProdutores} className={styles.selectProdutores}>
-                    <p>Produtores</p>
-                    <section className={styles.menuProdutores}>
-                        {produtores.map(produtor => {
-                            return <div onClick={ativaFiltroProdutor}>{produtor}</div>
-                        })}
-                    </section>
-                </section>
-                <a href="/carrinho">Carrinho</a>
-            </div>
-        </header>
-    )
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerConteudo}>
+        <img src="/icone-astral.png" alt="Astral logo" />
+        <a href="/">Início</a>
+        <a href="/sobre">Sobre</a>
+        <SearchBar />
+        <section
+          onMouseEnter={abrirMenuProdutores}
+          onMouseLeave={fecharMenuProdutores}
+          className={styles.selectProdutores}
+        >
+          <p>Produtores</p>
+          <section className={styles.menuProdutores}>
+            {produtores.map((produtor) => {
+              return <div onClick={ativaFiltroProdutor}>{produtor}</div>;
+            })}
+          </section>
+        </section>
+        <a href="/carrinho">
+          <TiShoppingCart size={24} color="#000" />
+          <p className={styles.precoCarrinho}>
+            <span>R$</span> 99,99
+          </p>
+        </a>
+      </div>
+    </header>
+  );
 }
