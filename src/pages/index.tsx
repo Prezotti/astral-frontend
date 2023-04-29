e949c9c391c765a2af3248ba9259b8f915c54427
Vinicius Cole de Amorim <viniamorim87@hotmail.com>
2023-04-29 10:28:04 -0300

    rota estava errada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,7 +109,7 @@ export default function Home() {
 
   useEffect(() => {
     api
-      .get("/produtos")
+      .get("/produto")
       .then((response) => {
         console.log(response.data);
       })
