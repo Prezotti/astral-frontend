2610ea1c7f36027483b8011b8ad8ceab36c86da6
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-05-06 22:28:17 -0300

    TR-027 Ajuste
    Ajustes na página do administrador:
    -Criação do css para o componente cardFeira;
    -Criação do botão swich para abrir a feira;
    -Alteração no html e css.

--- src/components/CardFeira.tsx
+++ src/components/CardFeira.tsx
@@ -1,33 +1,49 @@
 import { Button } from "@/components/Button";
-import styles from "../styles/pages/Admin.module.css";
-import {BiCalendar} from "react-icons/bi"
-import {RiMoneyDollarCircleLine} from "react-icons/ri"
-import {BsCircleFill} from "react-icons/bs"
+import styles from "../styles/components/CardFeira.module.css";
+import { BiCalendar } from "react-icons/bi";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { BsCircleFill } from "react-icons/bs";
 
-interface CardFeiraProps{
-     id : number;
-     aberta: boolean;
-     valorFinal : number;
-     data : string;
+interface CardFeiraProps {
+  id: number;
+  aberta: boolean;
+  valorFinal: number;
+  data: string;
 }
 
-export function CardFeira({id,aberta,valorFinal,data}: CardFeiraProps) {
+export function CardFeira({ id, aberta, valorFinal, data }: CardFeiraProps) {
   return (
     <div className={styles.divCard}>
       <div className={styles.esquerdaCard}>
         <h4>Feira {id}</h4>
-        <p className={styles.centralizar}><RiMoneyDollarCircleLine/> R$ {valorFinal.toFixed(2).replace('.',',')}</p>
+        <p className={styles.centralizar}>
+          <RiMoneyDollarCircleLine /> R${" "}
+          {valorFinal.toFixed(2).replace(".", ",")}
+        </p>
         <Button
           text="Consultar vendas"
           onClick={() => {}}
           classType="botaoCardFeira"
-          backgroundColor= "#72B234"
+          backgroundColor="#72B234"
         />
       </div>
 
       <div className={styles.direitaCard}>
-         {aberta ? <p className={styles.aberta} > <BsCircleFill color="#72B234"/> Aberta </p> : <p className={styles.fechado}> <BsCircleFill color="#FF3D3D"/> Fechado</p> }  
-        <p className={styles.centralizar}> <BiCalendar /> {data} </p>
+        {aberta ? (
+          <p className={styles.aberta}>
+            {" "}
+            <BsCircleFill color="#72B234" /> Aberta{" "}
+          </p>
+        ) : (
+          <p className={styles.fechado}>
+            {" "}
+            <BsCircleFill color="#FF3D3D" /> Fechado
+          </p>
+        )}
+        <p className={styles.centralizar}>
+          {" "}
+          <BiCalendar /> {data}{" "}
+        </p>
       </div>
     </div>
   );
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,45 +1,73 @@
 import { Button } from "@/components/Button";
 import styles from "../styles/pages/Admin.module.css";
 import { Header } from "@/components/Header";
-import {CardFeira} from "@/components/CardFeira";
+import { CardFeira } from "@/components/CardFeira";
+import { useState } from "react";
+import Switch from "@mui/material/Switch";
 
 export default function Admin() {
+  const [checked, setChecked] = useState(true);
+  const [textoSwitch, setTextoSwitch] = useState("Feira aberta!");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+    setTextoSwitch(event.target.checked ? "Feira aberta!" : "Abrir feira");
+  };
   return (
-    <body className="body">
+    <>
       <Header tipo="admin" />
-      <section className={styles.painelAdmin}>
-        <div className={styles.banner}>
-          <img
-            src="/banner-admin.jpg"
-            alt="Imagem de duas pessoas segurando um pote com tomates cerejas dentro"
-          />
-          <section className={styles.conteudo}>
-            <h1>Painel do Administrador</h1>
-            <p>O que você deseja fazer?</p>
-            <div className={styles.botoes}>
-              <Button
-                text="NOVA FEIRA"
-                onClick={() => {}}
-                classType="botaoBannerAdmin"
-              />
-              <Button
-                backgroundColor="#72B234"
-                text="CADASTRAR PRODUTOR"
-                onClick={() => {}}
-                classType="botaoBannerAdmin"
-              />
-            </div>
+      <section className={styles.body}>
+        <section className={styles.sectionPainel}>
+          <div className={styles.painel}>
+            <img
+              src="/banner-admin.jpg"
+              alt="Imagem de uma pessoa escolhendo uma verdura em um hortifruit"
+            />
+            <section className={styles.conteudo}>
+              <h1>Painel do Administrador</h1>
+              <p>O que você deseja fazer?</p>
+              <div className={styles.botoes}>
+                <Button
+                  text="NOVA FEIRA"
+                  onClick={() => {}}
+                  classType="botaoBannerAdmin"
+                />
+                <Button
+                  backgroundColor="#72B234"
+                  text="CADASTRAR PRODUTOR"
+                  onClick={() => {}}
+                  classType="botaoBannerAdmin"
+                />
+                <div className={styles.botaoSwitch}>
+                  <Switch
+                    color="warning"
+                    checked={checked}
+                    onChange={handleChange}
+                  />
+                  <p>{textoSwitch}</p>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+        <div className={styles.divFeira}>
+          <section className={styles.sectionFeira}>
+            <h1>Feiras</h1>
+            <CardFeira
+              id={2}
+              aberta={true}
+              valorFinal={910.15}
+              data="20/04/2023"
+            />
+            <CardFeira
+              id={1}
+              aberta={false}
+              valorFinal={910.15}
+              data="20/04/2023"
+            />
           </section>
         </div>
       </section>
-      <div className={styles.divFeira}>
-        <section className={styles.sectionFeira}>
-          <h1>Feiras</h1>
-          <CardFeira id={2} aberta={true} valorFinal={910.15} data="20/04/2023"/>
-          <CardFeira id={1} aberta={false} valorFinal={910.15} data="20/04/2023"/>
-        </section>
-      </div>
-
-    </body>
+    </>
   );
 }
