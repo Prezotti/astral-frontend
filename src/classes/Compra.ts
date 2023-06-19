f188741d984a0102e72e0727513eda51e149ce82
Mayara <mayarazanetticarlini@gmail.com>
2023-06-19 08:53:04 -0300

    TR-049 Incremento
    Criação da página de compra para guardar o CardPedido que é um componente, juntamente com o componente TabelaCardPedido. Além disso foram criadas as classes ListagemProduto e Compra.

---
+++ src/classes/Compra.ts
@@ -0,0 +1,98 @@
+import { ItemCompra } from "./ItemCompra"
+import { ListagemProduto } from "./ListagemProduto";
+
+export class Compra{
+    private _id: number;
+    private _data: Date;
+    private _cliente: string;
+    private _telefone: string;
+    private _endereco: string;
+    private _itens: ListagemProduto[];
+    private _formaPagamento: "" | "PICPAY" | "PIX" | "DINHEIRO";
+    private _localEntrega: "" | "ENTREGA" | "SANTA_TERESA" | "PATRIMONIO" | "IFES";
+    private _observacoes: string;
+    private _doacao: number;
+    private _valorTotal: number;
+    
+    constructor(id: number, data: Date, cliente: string, telefone: string, endereco: string, itens: ListagemProduto[], formaPagamento: "" | "PICPAY" | "PIX" | "DINHEIRO", localEntrega: "" |"ENTREGA" | "SANTA_TERESA" | "PATRIMONIO" | "IFES", observacoes: string, doacao: number, valorTotal: number){
+        this._id = id;
+        this._data = data;
+        this._cliente = cliente;
+        this._telefone = telefone;
+        this._endereco = endereco;
+        this._itens = itens;
+        this._formaPagamento = formaPagamento;
+        this._localEntrega = localEntrega;
+        this._observacoes = observacoes;
+        this._doacao = doacao;
+        this._valorTotal = valorTotal;
+    }
+    
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    public get data(): Date {
+        return this._data;
+    }
+    public set data(value: Date) {
+        this._data = value;
+    }
+    public get cliente(): string {
+        return this._cliente;
+    }
+    public set cliente(value: string) {
+        this._cliente = value;
+    }
+    public get telefone(): string {
+        return this._telefone;
+    }
+    public set telefone(value: string) {
+        this._telefone = value;
+    }
+    public get endereco(): string {
+        return this._endereco;
+    }
+    public set endereco(value: string) {
+        this._endereco = value;
+    }
+    public get itens(): ListagemProduto[] {
+        return this._itens;
+    }
+    public set itens(value: ListagemProduto[]) {
+        this._itens = value;
+    }
+    public get formaPagamento(): "" | "PICPAY" | "PIX" | "DINHEIRO" {
+        return this._formaPagamento;
+    }
+    public set formaPagamento(value: "" | "PICPAY" | "PIX" | "DINHEIRO") {
+        this._formaPagamento = value;
+    }
+    public get localEntrega(): "" | "ENTREGA" | "SANTA_TERESA" | "PATRIMONIO" | "IFES" {
+        return this._localEntrega;
+    }
+    public set localEntrega(value: "" | "ENTREGA" | "SANTA_TERESA" | "PATRIMONIO" | "IFES") {
+        this._localEntrega = value;
+    }
+    public get observacoes(): string {
+        return this._observacoes;
+    }
+    public set observacoes(value: string) {
+        this._observacoes = value;
+    }
+    public get doacao(): number {
+        return this._doacao;
+    }
+    public set doacao(value: number) {
+        this._doacao = value;
+    }
+    public get valorTotal(): number {
+        return this._valorTotal;
+    }
+    public set valorTotal(value: number) {
+        this._valorTotal = value;
+    }
+}
---
+++ src/components/CardPedido.tsx
@@ -0,0 +1,92 @@
+import styles from "../styles/components/CardPedido.module.css";
+import { BsCoin } from "react-icons/bs";
+import { BsTruck } from "react-icons/bs";
+import { RiCoinsLine } from "react-icons/ri";
+import { BsExclamation } from "react-icons/bs";
+import { MdKeyboardArrowUp } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useState } from "react";
+import { TabelaCardPedido } from "./TabelaCardPedido";
+import { Compra } from "@/classes/Compra";
+import { ListagemProduto } from "@/classes/ListagemProduto";
+import { Button } from "./Button";
+
+interface CardPedidoProps {
+  compra: Compra;
+}
+
+export function CardPedido({ compra }: CardPedidoProps) {
+  const [expandeContainer, setExpandeContainer] = useState(false);
+  const [mostraTabela, setMostraTabela] = useState(false);
+
+  const handleExpandeContainer = () => {
+    setExpandeContainer(!expandeContainer);
+    setMostraTabela(!mostraTabela);
+  };
+
+  const produtores: string[] = [];
+  compra.itens.forEach((item) => {
+    if (!produtores.includes(item.produtor)) {
+      produtores.push(item.produtor);
+    }
+  });
+
+  function formatarListaProdutos(produtor: string): ListagemProduto[] {
+    return compra.itens.filter((item) => item.produtor === produtor);
+  }
+
+  return (
+    <>
+      <div
+        className={`${styles.cardPedido} ${
+          expandeContainer ? styles.expandeContainer : ""
+        }`}
+      >
+        {" "}
+        <div className={styles.infoPedido}>
+          <div className={styles.pedido}>
+            <a href=""> <BsExclamation className={styles.iconeExclamacao} /></a>
+            <h3>Pedido #{compra.id}</h3>
+          </div>
+          <div className={styles.icones}>
+            <span>
+              <BsCoin className={styles.icone} />
+              {compra.formaPagamento}
+            </span>
+            <span>
+              <BsTruck className={styles.icone} />
+              {compra.localEntrega}
+            </span>
+            <span>
+              <RiCoinsLine className={styles.icone} />
+              Valor: R${compra.valorTotal}
+            </span>
+          </div>
+          <div className={styles.dadosComprador}>
+            <p>Comprador: {compra.cliente}</p>
+            <p>Telefone: {compra.telefone}</p>
+          </div>
+        </div>
+        {mostraTabela && (
+          <section className={styles.tabelas}>
+            {produtores.map((produtor) => {
+              return (
+                <TabelaCardPedido
+                  vetItensCompra={formatarListaProdutos(produtor)}
+                />
+              );
+            })}
+          </section>
+        )}
+        <div className={styles.verMais} onClick={handleExpandeContainer}>
+          <p>{expandeContainer ? "Ver Menos" : "Ver Mais"}</p>
+          {expandeContainer ? (
+            <MdKeyboardArrowUp className={styles.iconeSeta} />
+          ) : (
+            <MdKeyboardArrowDown className={styles.iconeSeta} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
