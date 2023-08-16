30eb4382eb2eab7dc3c6a2f075f1029eff99fa08
Henrique G. Prezotti <henriqueprezotti@gmail.com>
2023-08-16 17:10:09 -0300

    Requisicao https
    Foi alterada as requisições para o protocolo https

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-3-145-213-8.us-east-2.compute.amazonaws.com:8080",
+  baseURL: "https://api.ecowebfeira.com",
 });
 
 export default api;
